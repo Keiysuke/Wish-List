ba1e489ce9aa596eb128f79dda08a64ef0296f18
Thomas de Chazelles <T.dechazelles@hotmail.fr>
2021-11-12 00:17:38 +0100

    Adding product's photo on editing page

--- laravel8/resources/views/products/edit.blade.php
+++ laravel8/resources/views/products/edit.blade.php
@@ -5,6 +5,7 @@
 @endsection
 
 @section('content')
+@php($dir = config('images.path_products').'/'.$product->id.'/')
 <x-notification type="success" msg="{{ session('info') }}"/>
 
 <div class="min-w-full max-w-xs">
@@ -15,14 +16,17 @@
         <hr/>
         
         <div class="flex gap-4 mb-4">
-            <div <div class="w-3/6">
+            <div class="w-1/12 flex justify-center">
+                <img src="{{ asset($dir.$product->photos->firstWhere('ordered', 1)->label) }}" class="h-20"/>
+            </div>
+            <div <div class="w-5/12">
                 <x-form.label for="label" block required>Nom du produit</x-form.label>
                 <x-form.input name="label" placeholder="Uncharted 4" value="{{ old('label', $product->label) }}"/>
             </div>
-            <div class="flex justify-around w-3/6 gap-4">
+            <div class="flex justify-around w-6/12 gap-4">
                 <div class="w-1/2">
                     <x-form.label for="tag_ids" block required>Tags associés</x-form.label>
-                    <select multiple name="tag_ids[]" id="tag_ids" class="pl-2 h-10 block w-full rounded-md bg-gray-100 border-transparent">
+                    <select multiple name="tag_ids[]" id="tag_ids" class="pl-2 h-32 block w-full rounded-md bg-gray-100 border-transparent">
                         @foreach($tags as $tag)
                             <option value="{{ $tag->id }}" @if(in_array($tag->id, old('tags_id', $product->tag_ids()))) selected @endif>{{ $tag->label }}</option>
                         @endforeach
