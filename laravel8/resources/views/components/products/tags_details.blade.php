39b4d723292afb35b66365fc96371e85d816ebcd
Thomas de Chazelles <T.dechazelles@hotmail.fr>
2021-11-14 12:20:27 +0100

    Added Tags to product's lists

--- laravel8/resources/views/components/products/tags_details.blade.php
+++ laravel8/resources/views/components/products/tags_details.blade.php
@@ -1,5 +1,10 @@
-<div class="flex inline-flex gap-2">
+<div class="flex inline-flex gap-2 {{ $class ?? '' }}">
+    @php($i = 1)
 @foreach($tags as $tag)
+    @if(!($i++ % 4))
+        </div>
+        <div class="flex inline-flex gap-2 {{ $class ?? '' }}">
+    @endif
     <x-tags.tag :tag="$tag" />
 @endforeach
 </div>
--- laravel8/resources/views/partials/products/grid_details.blade.php
+++ laravel8/resources/views/partials/products/grid_details.blade.php
@@ -8,12 +8,15 @@
                         <div class="product_pict rounded rounded-b-none" style="background-image: url({{ asset(config('images.path_products').'/'.$product->id.'/'.$product->photos()->firstWhere('ordered', 1)->label) }})"></div>
                     </div>
                     <div class="down flex flex-col justify-between h-full font-semibold text-center {{ ($product->nb_offers > 0)? ($product->can_buy? '' : 'offer-soon' ) : 'no-offer' }}">
-                        <p class="mt-2 text-black">{{ (strlen($product->label) > 50)? substr($product->label, 0, 50).'...' : $product->label }}</p>
+                        <p class="my-2 text-black">{{ (strlen($product->label) > 50)? substr($product->label, 0, 50).'...' : $product->label }}</p>
+                        
+                        <x-products.tags_details class="mb-2 justify-center" :tags="$product->tags"/>
+                        
                         <div class="flex justify-end items-end">
                             @if($product->archived)
                                 <x-svg.archive class="icon-sm absolute left-1 bottom-1 text-yellow-700"/>
                             @endif
-                            <p class="text-xs bg-gray-800 text-white mt-3 p-2">
+                            <p class="text-xs bg-gray-800 text-white p-2">
                                 @if($product->nb_offers > 0 && !$product->can_buy)
                                     <svg class="h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
